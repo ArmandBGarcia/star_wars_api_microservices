a80210f32f88cb9f26ce5f1326b70be138f5d7fe
ArmandBGarcia <xdarcx@hotmail.es>
2023-07-13 16:56:17 -0600

    adding cors

--- gateway/server.js
+++ gateway/server.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors({ origin: "*" }));
 
 app.use(
   "/characters",
